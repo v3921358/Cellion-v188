9732f8b45cf60ab1e7944fd616add0a564d7355a
Mazen <mazen.inquiries@gmail.com>
2018-05-08 20:54:50 -0400

    Updated Tempest Blades handler
    Updated Tempest Blades handler, I believe a bit more work is required regarding temporary stats to get it working fully.

--- Nox/Core/src/client/jobs/Nova.java
+++ Nox/Core/src/client/jobs/Nova.java
@@ -3,18 +3,37 @@
  */
 package client.jobs;
 
+import client.CharacterTemporaryStat;
 import client.SkillFactory;
+import constants.skills.Kaiser;
 import server.maps.objects.User;
 
 /**
  * Nova Class Handlers
  *
- * @author Mazen
+ * @author Mazen Massoud
  */
 public class Nova {
 
     public static class KaiserHandler {
 
+        public static int getTempestBladeSkill(User pPlayer) {
+            int nSkillID = 0;
+            if(pPlayer.hasSkill(Kaiser.TEMPEST_BLADES_1)) {
+                nSkillID = Kaiser.TEMPEST_BLADES_1;
+            }
+            if(pPlayer.hasSkill(Kaiser.TEMPEST_BLADES) && pPlayer.hasBuff(CharacterTemporaryStat.Morph)) {
+                nSkillID = Kaiser.TEMPEST_BLADES;
+            }
+            if(pPlayer.hasSkill(Kaiser.ADVANCED_TEMPEST_BLADES_1)) {
+                nSkillID = Kaiser.ADVANCED_TEMPEST_BLADES_1;
+            }
+            if(pPlayer.hasSkill(Kaiser.ADVANCED_TEMPEST_BLADES) && pPlayer.hasBuff(CharacterTemporaryStat.Morph)) {
+                nSkillID = Kaiser.ADVANCED_TEMPEST_BLADES;
+            }
+            return nSkillID;
+        }
+        
         public static void handleKaiserCombo(User pPlayer) {
             if (pPlayer.getPrimaryStack() < 1000) {
                 pPlayer.setPrimaryStack(pPlayer.getPrimaryStack() + 3);
--- Nox/Core/src/handling/game/ReleaseTempestBlades.java
+++ Nox/Core/src/handling/game/ReleaseTempestBlades.java
@@ -13,14 +13,22 @@ import tools.packet.CWvsContext;
 import net.InPacket;
 import net.ProcessPacket;
 import client.CharacterTemporaryStat;
+import client.jobs.Nova.KaiserHandler;
+import constants.skills.Kaiser;
+import java.awt.Point;
+import java.awt.Rectangle;
 import server.MapleStatEffect;
 import java.util.ArrayList;
 import java.util.List;
+import org.w3c.dom.css.Rect;
+import server.life.Mob;
+import server.maps.objects.ForceAtom;
+import server.maps.objects.ForceAtomType;
 import tools.Pair;
 import server.maps.objects.User;
 
 /**
- *
+ * 
  * @author Mazen
  */
 public final class ReleaseTempestBlades implements ProcessPacket<MapleClient> {
@@ -32,28 +40,74 @@ public final class ReleaseTempestBlades implements ProcessPacket<MapleClient> {
 
     @Override
     public void Process(MapleClient c, InPacket iPacket) {
-        final User chr = c.getPlayer();
-        if (!chr.isAlive()) {
-            chr.getClient().SendPacket(CWvsContext.enableActions());
-            return;
+        User pPlayer = c.getPlayer();
+        int nMaxCount = 3;
+        if (KaiserHandler.getTempestBladeSkill(pPlayer) == Kaiser.ADVANCED_TEMPEST_BLADES_1 || KaiserHandler.getTempestBladeSkill(pPlayer) == Kaiser.ADVANCED_TEMPEST_BLADES) {
+            nMaxCount = 5;
         }
-        final int mobcount = iPacket.DecodeInt();
-        final List<Integer> oids = new ArrayList<>();
-        for (int i = 0; i < mobcount; i++) {
-            oids.add(iPacket.DecodeInt());
-        }
-        int skillid = chr.getBuffSource(CharacterTemporaryStat.StopForceAtomInfo);
+        int mobCount = iPacket.DecodeInt();
+        int lastMobID = 0;
+        int nMobID = 0;
 
-        List<Pair<Integer, Integer>> forceinfo = new ArrayList<>();
-        boolean advanced = skillid == 61120007 || skillid == 61121217;
-        boolean transform = skillid == 61110211 || skillid == 61121217;
-        for (int i = 0; i < (advanced ? 5 : 3); i++) {
-            forceinfo.add(new Pair<>(i + 2, transform ? 4 : 2));
-        }
-        chr.getMap().broadcastMessage(CField.gainForce(false, chr, oids, 2, skillid, forceinfo, null, 0));
+        for (int i = 0; i < mobCount; i++) {
+            nMobID = iPacket.DecodeInt();
 
-        MapleStatEffect skill_effect = SkillFactory.getSkill(skillid).getEffect(chr.getTotalSkillLevel(skillid));
-        chr.cancelEffect(skill_effect, true, 0);
-        chr.cancelTemporaryStats(CharacterTemporaryStat.StopForceAtomInfo);
+            Mob pMob = pPlayer.getMap().getMonsterByOid(nMobID);
+            int nInc = ForceAtomType.KAISER_WEAPON_THROW_1.getInc();
+            int nType = ForceAtomType.KAISER_WEAPON_THROW_1.getForceAtomType();
+
+            switch (pPlayer.getBuffedValue(CharacterTemporaryStat.StopForceAtomInfo)) {
+                case 3:
+                    nInc = ForceAtomType.KAISER_WEAPON_THROW_MORPH_1.getInc();
+                    nType = ForceAtomType.KAISER_WEAPON_THROW_MORPH_1.getForceAtomType();
+                    break;
+                case 2:
+                    nInc = ForceAtomType.KAISER_WEAPON_THROW_2.getInc();
+                    nType = ForceAtomType.KAISER_WEAPON_THROW_2.getForceAtomType();
+                    break;
+                case 4:
+                    nInc = ForceAtomType.KAISER_WEAPON_THROW_MORPH_2.getInc();
+                    nType = ForceAtomType.KAISER_WEAPON_THROW_MORPH_2.getForceAtomType();
+                    break;
+            }
+            
+            ForceAtom forceAtomInfo = new ForceAtom(1, nInc, 25, 30, 0, 10 * i, (int) System.currentTimeMillis(), 1, 0, new Point());
+            pPlayer.getMap().broadcastMessage(CField.createForceAtom(false, 0, pPlayer.getId(), nType,
+                    true, pMob.getObjectId(), KaiserHandler.getTempestBladeSkill(pPlayer), forceAtomInfo, new Rectangle(), 0, 300,
+                    pMob.getPosition(), KaiserHandler.getTempestBladeSkill(pPlayer), pMob.getPosition()));
+
+            lastMobID = nMobID;
+        }
+
+        for (int i = mobCount; i < nMaxCount; i++) {
+
+            Mob pMob = pPlayer.getMap().getMonsterByOid(lastMobID);
+            int nInc = ForceAtomType.KAISER_WEAPON_THROW_1.getInc();
+            int nType = ForceAtomType.KAISER_WEAPON_THROW_1.getForceAtomType();
+
+            switch (pPlayer.getBuffedValue(CharacterTemporaryStat.StopForceAtomInfo)) {
+                case 3:
+                    nInc = ForceAtomType.KAISER_WEAPON_THROW_MORPH_1.getInc();
+                    nType = ForceAtomType.KAISER_WEAPON_THROW_MORPH_1.getForceAtomType();
+                    break;
+                case 2:
+                    nInc = ForceAtomType.KAISER_WEAPON_THROW_2.getInc();
+                    nType = ForceAtomType.KAISER_WEAPON_THROW_2.getForceAtomType();
+                    break;
+                case 4:
+                    nInc = ForceAtomType.KAISER_WEAPON_THROW_MORPH_2.getInc();
+                    nType = ForceAtomType.KAISER_WEAPON_THROW_MORPH_2.getForceAtomType();
+                    break;
+            }
+            
+            ForceAtom forceAtomInfo = new ForceAtom(1, nInc, 25, 30, 0, 12 * i, (int) System.currentTimeMillis(), 1, 0, new Point());
+            pPlayer.getMap().broadcastMessage(CField.createForceAtom(false, 0, pPlayer.getId(), nType,
+                    true, pMob.getObjectId(), KaiserHandler.getTempestBladeSkill(pPlayer), forceAtomInfo, new Rectangle(), 0, 300,
+                    pMob.getPosition(), KaiserHandler.getTempestBladeSkill(pPlayer), pMob.getPosition()));
+        }
+        
+        MapleStatEffect pEffect = SkillFactory.getSkill(KaiserHandler.getTempestBladeSkill(pPlayer)).getEffect(pPlayer.getTotalSkillLevel(KaiserHandler.getTempestBladeSkill(pPlayer)));
+        pPlayer.cancelEffect(pEffect, true, 0);
+        pPlayer.cancelTemporaryStats(CharacterTemporaryStat.StopForceAtomInfo);
     }
 }
